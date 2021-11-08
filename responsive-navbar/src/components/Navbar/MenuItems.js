caf9d82c1eb955be2e9bfb312236b19eb6885bcd
AkshayBenny <akshaybennygh@gmail.com>
2021-11-08 09:39:03 +0530

    update

---
+++ responsive-navbar/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Portfolio",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Contacts",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign Up",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
